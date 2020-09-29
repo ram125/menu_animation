ab33bb0ce3276e9fcd0c089510a681665454461e
Roman Dain <ram.123.guitar@gmail.com>
2020-09-29 21:51:30 +0300

    added the more complicated but more smooth version. no more bugs. probably will optimise later

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let btn = document.getElementById("menu_btn")
-let body = document.getElementById("menu_body")
 btn.addEventListener("click", ()=>{
-	if(body.classList.contains("active")){
-		body.classList.remove("active")
+	if(btn.classList.contains("active")){
+		btn.classList.remove("active")
 	}else{
-		body.classList.add("active")
+		btn.classList.add("active")
 	}
 })
